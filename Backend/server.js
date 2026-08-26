9f392aa31e4e4d7d8878c018985b4201f3876e3b
MuhammadLuqman0407 <muhammadluqman8303@gmail.com>
2026-08-26 21:40:39 +0500

    configuration

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+// allow multipe origins
+const allowedOrigins = ['http://localhost:5173']
+
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin:allowedOrigins, credentials:true}));
+
 
 app.get('/', (req,res) => {
     res.send("Api is working ");
